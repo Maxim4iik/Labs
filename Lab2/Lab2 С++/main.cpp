2db5e28106aa7fea98e713bcb07fa0d94ad6dcf8
Максим Трохимец <trokhimets0402@gmail.com>
2020-03-16 15:18:47 +0200

    Update main.cpp

--- Lab2/Lab2 С++/main.cpp
+++ Lab2/Lab2 С++/main.cpp
@@ -19,36 +19,36 @@ int main() {
   cout << "Empty:" << endl << text << endl;
 
   cout << endl;
-  text.Add("Far far 1 away, 2 behind the 3 word mountains");
-  cout << ".Add() x1:" << endl << text << endl;
+  text.Add(" 1 away, 2 behind, 3 words");
+  cout << "Add() x1:" << endl << text << endl;
 
   cout << endl;
-  text.Add("Far from 4 the countries 5 Vokalia and Consonantia");
-  text.Add("There live 6 the blind texts");
-  text.Add("Separated they 7 live in 8 Bookmarksgrove");
-  text.Add("Right at 9 the coast of the Semantics, a 0 large language ocean");
-  cout << ".Add() x4:" << endl << text << endl;
+  text.Add(" Far from 4 the countries 5");
+  text.Add(" There live 6 old men");
+  text.Add(" Separated they 7 live in 8 Bookmarksgrove");
+  text.Add(" Right at 9, there is a 0 large ocean");
+  cout << "Add() x4:" << endl << text << endl;
 
   cout << endl;
-  cout << ".Digits(): " << text.Digits() << endl;
+  cout << "Digits(): " << text.Digits() << endl;
 
   cout << endl;
-  text.Replace("Separated they 7 live in 8 Bookmarksgrove", "hello world");
-  cout << ".Replace():" << endl << text << endl;
+  text.Replace(" Separated they 7 live in 8 Bookmarksgrove", " hello world");
+  cout << "Replace():" << endl << text << endl;
 
   cout << endl;
-  cout << ".Size(): " << text.Size() << endl;
+  cout << "Size(): " << text.Size() << endl;
 
   cout << endl;
-  text.Remove("There live 6 the blind texts");
-  cout << ".Remove():" << endl << text << endl;
+  text.Remove(" There live 6 old men");
+  cout << "Remove():" << endl << text << endl;
 
   cout << endl;
-  cout << ".Size(): " << text.Size() << endl;
+  cout << "Size(): " << text.Size() << endl;
 
   cout << endl;
   text.Clear();
-  cout << ".Clear():" << endl << text << endl;
+  cout << "Clear():" << endl << text << endl;
 
   return 0;
 }
